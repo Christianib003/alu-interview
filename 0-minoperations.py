4b26f1f104b951c7ee977dc8cefa77b7ad2680b3
Christianib003 <christianib003@gmail.com>
2024-05-19 11:38:14 +0200

    implement solution

--- 0-minoperations.py
+++ 0-minoperations.py
@@ -1,2 +1,18 @@
 #! /usr/bin/python3
 
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    current_num_of_h = 1
+    copied = 0
+    num_of_operations = 0
+
+    while current_num_of_h < n:
+        if n % current_num_of_h == 0:
+            copied = current_num_of_h
+            num_of_operations += 1
+
+        current_num_of_h += copied
+        num_of_operations += 1
+    return num_of_operations
